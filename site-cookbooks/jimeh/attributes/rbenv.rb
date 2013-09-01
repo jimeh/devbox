f20c5f54dbf61917e82be544768f0bd65abbbd7a
Jim Myhrberg <contact@jimeh.me>
2013-09-01 08:43:55 +0100

    Switch rbenv cookbook to one that support user-account installation

---
+++ site-cookbooks/jimeh/attributes/rbenv.rb
@@ -0,0 +1,15 @@
+default['rbenv']['user_installs'] = [
+  {
+    'user'    => 'jimeh',
+    'rubies'  => ['1.9.3-p448'],
+    'global'  => '1.9.3-p448',
+    'gems'    => {
+      '1.9.3-p448' => [
+        {'name' => 'bundler'},
+        {'name' => 'rake'},
+        {'name' => 'foreman'},
+        {'name' => 'rbenv-rehash'}
+      ]
+    }
+  }
+]
--- site-cookbooks/jimeh/recipes/packages.rb
+++ site-cookbooks/jimeh/recipes/packages.rb
@@ -16,19 +16,5 @@ include_recipe "tmux::source"
 include_recipe "docker-ubuntu"
 include_recipe "emacs24-ppa"
 include_recipe "heroku-toolbelt"
-
-
-#
-# Ruby and related tools
-#
-
-rbver = "1.9.3-p448"
-include_recipe "rbenv"
-include_recipe "rbenv::ruby_build"
-
-rbenv_ruby rbver
-
-gems = ["foreman"]
-gems.each do |gemname|
-  rbenv_gem(gemname) { ruby_version rbver }
-end
+include_recipe "ruby_build"
+include_recipe "rbenv::user"
