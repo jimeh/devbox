8809f3a8f6c8903f3a14bbcdd1356ffcaab82148
Jim Myhrberg <contact@jimeh.me>
2013-09-23 03:15:06 +0100

    Update installed ruby versions

--- site-cookbooks/jimeh/attributes/rbenv.rb
+++ site-cookbooks/jimeh/attributes/rbenv.rb
@@ -1,16 +1,17 @@
+base_gems = [
+  {'name' => 'foreman'},
+  {'name' => 'rbenv-rehash'},
+  {'name' => 'git_pissed'}
+]
+
 default['rbenv']['user_installs'] = [
   {
-    'user'    => 'jimeh',
-    'rubies'  => ['1.9.3-p448'],
-    'global'  => '1.9.3-p448',
-    'gems'    => {
-      '1.9.3-p448' => [
-        {'name' => 'bundler'},
-        {'name' => 'rake'},
-        {'name' => 'foreman'},
-        {'name' => 'rbenv-rehash'},
-        {'name' => 'git_pissed'}
-      ]
+    'user'   => 'jimeh',
+    'rubies' => ['1.9.3-p448', '2.0.0-p247'],
+    'global' => '1.9.3-p448',
+    'gems'   => {
+      '1.9.3-p448' => base_gems,
+      '2.0.0-p247' => base_gems
     }
   }
 ]
